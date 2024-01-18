f5598a4fc904f91b1c5c532cacd135936200f921
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2024-01-18 23:18:41 +0400

    css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { Container, Typography, Link } from '@mui/material';
+import { Container, Typography, Link, Grid } from '@mui/material';
+import './styles.css'; 
 
 const App = () => {
   return (
     <Container>
-      <Typography variant="h1" color="primary">
-        Welcome to my notes on technology
-      </Typography>
-      <Typography variant="body1">
-        This is a simple website to publish my notes.
-      </Typography>
-      <Link href="Java.md" color="success" underline="hover" fontWeight="bold">
-        java important notes
-      </Link>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4" color="primary">
+            Welcome to my notes on technology
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            This is a simple website to publish my notes.
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Link href="Java.md" color="success" underline="hover" fontWeight="bold">
+            Java important notes
+          </Link>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
